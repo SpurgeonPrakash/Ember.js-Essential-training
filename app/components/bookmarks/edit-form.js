fbfa7bf028066b058236e19c5b25a99894820c28
mamriarti <mamriarti@gmail.com>
2017-05-11 18:56:14 +0300

    add a new record 8.2

--- app/components/bookmarks/edit-form.js
+++ app/components/bookmarks/edit-form.js
@@ -1,8 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	actions: {
-		save(bookmark){ Ember.Logger.info('save called'); },
-	    cancel(){ Ember.Logger.info('Cancel called'); }
-	}
+  bookmark: null,
+  router: Ember.inject.service('-routing'),
+
+  actions: {
+    save(bookmark) { Ember.Logger.info('save called');
+    bookmark.save()
+    .then(
+      (value) => {
+        Ember.Logger.info(value);
+        this.get('router').transitionTo('bookmarks');
+      }
+    )
+    .catch(
+      () => {
+        Ember.Logger.info('failure to save');
+      }
+    )
+  },
+    cancel() { Ember.Logger.info('Cancel called'); }
+  }
 });
+
